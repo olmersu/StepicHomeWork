5c4f529a916108be4c120f852769f5c37ed911e2
olmersu <olmersu@gmail.com>
2016-09-30 23:03:38 +0800

    23.09.2016 Home

--- src/com/Algorithms/DivideAndConquer/countsort/CountSort.java
+++ src/com/Algorithms/DivideAndConquer/countsort/CountSort.java
@@ -3,6 +3,7 @@ package com.Algorithms.DivideAndConquer.countsort;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Created by bogomolov on 30.09.2016.
@@ -18,42 +19,39 @@ public class CountSort {
     private void run() {
 //        Scanner in = new Scanner(System.in);
 //        int arrCount = in.nextInt();
-        int maxNum = 5;
-        int arrCount = 20;
+        int maxNum = 10;
+        int arrCount = 10000;
         int[] arr = new int[arrCount];
-        //        for (int i = 0; i < arrCount; i++) {
+//        for (int i = 0; i < arrCount; i++) {
 //            arr[i] = in.nextInt();
-//            arr3[i]=arr[i];
 //        }
-        Random rand = new Random();
-        for (int i = 0;i < arrCount; i++) {
-            int num = rand.nextInt(arrCount);
-            arr[i] = num;
-            System.out.print(arr[i] + " ");
+            Random rand = new Random();
+            for (int i = 0; i < arrCount; i++) {
+                int num = rand.nextInt(maxNum);
+                arr[i] = num;
+//                System.out.print(arr[i] + " ");
+            }
+//            System.out.println("\n");
+            arr = sort(arr, maxNum);
+//            for (int i = 0; i < arrCount; i++) {
+//                System.out.print(arr[i] + " ");
+//            }
+//            System.out.println("\n");
         }
-        System.out.println("\n");
-        arr=sort(arr, maxNum);
-        for (int i = 0;i < arrCount; i++) {
-            int num = rand.nextInt(arrCount);
-            arr[i] = num;
-            System.out.print(arr[i] + " ");
+        private int[] sort ( int[] arr, int maxNum){
+            int[] resArr = new int[arr.length];
+            int[] arrB = new int[maxNum];
+            Arrays.fill(arrB, 0);
+            for (int i = 0; i < arr.length; i++) {
+                arrB[arr[i]] += 1;
+            }
+            for (int i = 1; i < maxNum; i++) {
+                arrB[i] += (arrB[i - 1]);
+            }
+            for (int i = arr.length - 1; i >= 0; i--) {
+                resArr[arrB[arr[i]] - 1] = arr[i];
+                arrB[arr[i]] -= 1;
+            }
+            return resArr;
         }
-        System.out.println("\n");
     }
-    private int[] sort(int[] arr, int maxNum){
-        int [] resArr = new int[arr.length];
-        int [] arrB = new int[maxNum];
-        Arrays.fill(arrB, 0);
-        for (int i = 0; i < arr.length; i++) {
-            arrB[arr[i]]+=1;
-        }
-        for (int i = 1; i < maxNum; i++) {
-            arrB[i]+=arrB[i-1];
-        }
-        for (int i = arr.length-1; i >= 0; i--) {
-            resArr[arrB[arr[i]]]=arr[i];
-            arrB[arr[i]]-=1;
-        }
-        return resArr;
-    }
-}
